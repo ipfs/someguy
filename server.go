ef69b64a9262c9a6da4ed9e3f8daece418c5d745
Daniel N <2color@users.noreply.github.com>
2024-11-14 10:35:32 +0100

    chore: remove request timeout
    this isn't working too well. We need to look more deeply into this

--- server.go
+++ server.go
@@ -132,9 +132,6 @@ func start(ctx context.Context, cfg *config) error {
 	// Add request logging.
 	handler = withRequestLogger(handler)
 
-	// Add request timeout
-	handler = http.TimeoutHandler(handler, 30*time.Second, "Request timed out")
-
 	// Add request tracing
 	handler = withTracingAndDebug(handler, cfg.tracingAuth)
 
